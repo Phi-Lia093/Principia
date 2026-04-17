3bd94df8cac313b13a558902389e79167a9aa2ca
Phi-Lia093 <aiyu2009g@gmail.com>
2026-04-17 19:35:09 +0800

    env setup

---
+++ src/main/java/com/philia093/Principia.java
@@ -0,0 +1,15 @@
+package com.philia093;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Principia implements ModInitializer {
+	public static final String MOD_ID = "principia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
