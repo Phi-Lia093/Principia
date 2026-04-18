068952bf8ef52a83bd8571fc3c32a93ab3b51978
Phi-Lia093 <aiyu2009g@gmail.com>
2026-04-19 00:49:35 +0800

    add ores

--- src/main/java/com/philia093/blocks/ModBlocks.java
+++ src/main/java/com/philia093/blocks/ModBlocks.java
@@ -12,8 +12,45 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
-    public static final Block ZINC_ORE = registerBlocks("zinc_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f, 3.0f)));
+    // ores
+    public static final Block APATITE_ORE = registerBlocks("apatite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block BAUXITE_ORE = registerBlocks("bauxite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block CASSITERITE_ORE = registerBlocks("cassiterite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(6.0f)));
+    public static final Block CHALCOPYRITE_ORE = registerBlocks("chalcopyrite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block CHROMITE_ORE = registerBlocks("chromite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE).strength(6.0f)));
+    public static final Block FLUORITE_ORE = registerBlocks("fluorite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block GALENA_ORE = registerBlocks("galena_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block ILMENITE_ORE = registerBlocks("ilmenite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE).strength(6.0f)));
+    public static final Block LIGNITE_ORE = registerBlocks("lignite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.STONE).strength(1.0f)));
+    public static final Block LIMESTONE_ORE = registerBlocks("limestone_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block MAGNETITE_ORE = registerBlocks("magnetite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block MALACHITE_ORE = registerBlocks("malachite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block MONAZITE_ORE = registerBlocks("monazite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE).strength(6.0f)));
+    public static final Block PENTLANDITE_ORE = registerBlocks("pentlandite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE).strength(3.0f)));
+    public static final Block PYROLUSITE_ORE = registerBlocks("pyrolusite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block ROCK_SALT_ORE = registerBlocks("rock_salt_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block SPHALERITE_ORE = registerBlocks("sphalerite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3.0f)));
+    public static final Block SULFUR_ORE = registerBlocks("sulfur_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(1.0f)));
+    public static final Block VANADINITE_ORE = registerBlocks("vanadinite_ore",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(4.0f)));
 
     private static Block registerBlocksWithoutItem(String name, Block block) {
         return Registry.register(Registries.BLOCK, new Identifier(Principia.MOD_ID, name), block);
--- src/main/java/com/philia093/items/ModItemGroups.java
+++ src/main/java/com/philia093/items/ModItemGroups.java
@@ -10,13 +10,31 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroups {
-    public static final ItemGroup MATERIALS = Registry.register(Registries.ITEM_GROUP,
-            new Identifier(Principia.MOD_ID, "materials"),
+    public static final ItemGroup ORES = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(Principia.MOD_ID, "ores"),
             ItemGroup.create(null, -1)
-                    .displayName(Text.translatable("itemGroup.materials"))
-                    .icon(() -> new ItemStack(ModBlocks.ZINC_ORE))
+                    .displayName(Text.translatable("itemGroup.ores"))
+                    .icon(() -> new ItemStack(ModBlocks.APATITE_ORE))
                     .entries((displayContext, entries) -> {
-                        entries.add(ModBlocks.ZINC_ORE);
+                        entries.add(ModBlocks.APATITE_ORE);
+                        entries.add(ModBlocks.BAUXITE_ORE);
+                        entries.add(ModBlocks.CASSITERITE_ORE);
+                        entries.add(ModBlocks.CHALCOPYRITE_ORE);
+                        entries.add(ModBlocks.CHROMITE_ORE);
+                        entries.add(ModBlocks.FLUORITE_ORE);
+                        entries.add(ModBlocks.GALENA_ORE);
+                        entries.add(ModBlocks.ILMENITE_ORE);
+                        entries.add(ModBlocks.LIGNITE_ORE);
+                        entries.add(ModBlocks.LIMESTONE_ORE);
+                        entries.add(ModBlocks.MAGNETITE_ORE);
+                        entries.add(ModBlocks.MALACHITE_ORE);
+                        entries.add(ModBlocks.MONAZITE_ORE);
+                        entries.add(ModBlocks.PENTLANDITE_ORE);
+                        entries.add(ModBlocks.PYROLUSITE_ORE);
+                        entries.add(ModBlocks.ROCK_SALT_ORE);
+                        entries.add(ModBlocks.SPHALERITE_ORE);
+                        entries.add(ModBlocks.SULFUR_ORE);
+                        entries.add(ModBlocks.VANADINITE_ORE);
                     }).build());
 
     public static void registerModItemGroups(){
